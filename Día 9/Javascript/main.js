b855f92f48cf64bf3aed803ecee467581c875292
Jorge <jorgesalas@MacBook-de-Jorge.local>
2020-01-31 22:04:10 -0600

    Trabajo del Dia 9

---
+++ Día 9/Javascript/main.js
@@ -0,0 +1,11 @@
+/* Crea una función llammada suma que reciba dos parámetros, a y b, y regrese la suma de ambos números. 
+Recuerda que para devolver el valor de una función se utiliza la palabra reservada "return" */
+
+function suma (a, b){
+
+    let resultado = a + b;
+    console.log(resultado);
+
+}
+
+suma(4,7);
